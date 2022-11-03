c66d4a12061263be3b4c0b03ca7f1870aff653c3
Rozan Ghosani <18221121@mahasiswa.itb.ac.id>
2022-11-03 14:12:18 +0700

    Changes in save, fix mesin untuk File

--- src/playGame.h
+++ src/playGame.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "./ADT/queue.h"
 
-void playGame(TabKata* queueGame);
+void playGame(Queue* queueGame);
 /* Memainkan game yang ada pada defaultGame
 I.S. queueGame terdefinisi, mungkin kosong
 F.S. Game teratas dimainkan
