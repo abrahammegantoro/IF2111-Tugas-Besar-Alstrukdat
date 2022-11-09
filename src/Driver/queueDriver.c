60a1398e6365ff2503bf3b268fb10be6382dc901
Josua Adriel Sinabutar <18221065@std.stei.itb.ac.id>
2022-11-09 23:09:55 +0700

    Update queueDriver.c

--- src/Driver/queueDriver.c
+++ src/Driver/queueDriver.c
@@ -1,17 +1,25 @@
+# include <stdio.h>
 # include "../ADT/queue.h"
 
 int main(){
-    STARTINPUT();
     Queue q;
     CreateQueue(&q);
-    enqueue(&q, currentWord);
-    printf("udah sekali");
-    STARTINPUT();
-    enqueue(&q, currentWord);
     if (isEmpty(q)){
-        printf("kosong");
-    }else{
-        printf("%d", length(q));
+        printf("kosong\n"); //bukti is empty menunjukkan queue kosong
+    }
+    printf("Masukan kata:");
+    STARTINPUT();
+    while(!WordCompare(currentWord, toKata("Keluar"))){
+        enqueue(&q, currentWord); //memasukan kata ke dalam queue
+        printf("Berhasil melakukan queue\n");
+        printf("Masukan kata lagi:");
+        STARTINPUT();
+    }
+    while(!isEmpty(q)){
+        printf("Banyak antrian: %d\n", length(q)); 
+        dequeue(&q, &currentWord); //mengeluarkan kata dari dalam queue dengan sistem FIFO
+        PrintWord(currentWord);
+        printf("\n");
     }
     return 0;
 }
