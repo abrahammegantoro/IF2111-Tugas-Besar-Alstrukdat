6b507c91dca0105ca5552ad348dc6e48f8c9e0a7
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2022-11-28 20:52:22 +0700

    Update reset scoreboard

--- src/resetScoreboard.c
+++ src/resetScoreboard.c
@@ -8,8 +8,8 @@ void resetScoreboard(TabKata x, ListScore *scoreBoard){
         printf("  %d. ", k+1);
         k++;
         for(int j = 0; j < x.TI[i].Length; j++) {
-        printf("%c", x.TI[i].TabWord[j]);
-    }
+            printf("%c", x.TI[i].TabWord[j]);
+        }
     printf("\n");
     }
     printf("SCOREBOARD YANG INGIN DIHAPUS: ");
@@ -20,17 +20,29 @@ void resetScoreboard(TabKata x, ListScore *scoreBoard){
     if (k == 0){
         printf("APAKAH KAMU YAKIN INGIN MELAKUKAN RESET SCOREBOARD ALL (YA/TIDAK)? ");
         STARTINPUT();
+        Word input = currentWord;
         printf("\n");
-        if (WordCompare(toKata("YA"), currentWord)){
-            for (j = 0; j < x.Neff; j++){
-              CreateListScore(&scoreBoard[j]);
+        ADVWORD();
+        if (EndWord){
+            if (WordCompare(toKata("YA"), input)){
+                for (j = 0; j < x.Neff; j++){
+                    CreateListScore(&scoreBoard[j]);
+                }
+                printf("Scoreboard berhasil di-reset.\n");
+            } else if (WordCompare(toKata("TIDAK"), input)){
+                printf("Scoreboard tidak jadi di-reset.\n");
+            } else {
+                printf("Input tidak valid.\n");
             }
+        } else {
+            printf("Input tidak valid.\n");
         }
-        printf("Scoreboard berhasil di-reset.\n");
-    } else {
+    } else if (k >= 1 && k <= x.Neff) {
         CreateEmptySet(&(scoreBoard[k-1]).Nama);
         CreateEmptyMap(&(scoreBoard[k-1]).Score);
         printf("Scoreboard berhasil di-reset.\n");
+    } else {
+        printf("Input tidak valid.\n");
     }
 };
 
